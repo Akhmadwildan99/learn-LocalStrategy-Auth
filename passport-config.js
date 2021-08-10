289b51826dcfeda0e8d6374838ddfa50c6dd0347
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-08-11 04:06:15 +0700

    menambah file passport-config untuk mengeset local strategy

---
+++ passport-config.js
@@ -0,0 +1,29 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt')
+
+function initialize(passport, getByEmail, getById) {
+    const authenticateUser = async (email, password, done) => {
+        const user = getByEmail(email);
+        if(user == null) {
+            return done(null, false, {message: 'No user with that email!'})
+        }
+
+        try{
+            if( await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else{
+                return done(null, false , {message: 'Password Incorect'})
+            }
+        } catch(e){
+            return done(e)
+        }
+    }
+
+    passport.user( new LocalStrategy({usernameField: 'email'}, authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+        return done(null, getById(id))
+    })
+}
+
+module.exports = initialize
